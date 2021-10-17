3740cf6564df5d95d86c9f39f2539ec24f7e2fc5
Allidjinou Michée Josué <allidjinoumichee@outlook.fr>
2021-10-17 10:27:16 +0000

    Change welcome message

--- index.js
+++ index.js
@@ -10,7 +10,38 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the unofficial AvaxNews API !')
+  res.send(`
+  # avaxnewsapi
+_An express js api for fetching recent articles from avax.news_
+
+## Dependencies
+The API rely on :
+* express js for the server
+* puppeteer for web scraping
+
+## Basic usage
+You can fetch recent articles per category. The API endpoint for that is _/category/:category_. The response is a JSON object containing the title, the cover image, the description and the link of the article. 
+Example :
+
+/category/fact
+
+
+## Deployed version
+You can test it here : https://avaxnewsapi.herokuapp.com/
+
+## Articles category available
+* fact
+* wow
+* charming
+* ohlala
+* touching
+* funny
+* educative
+* sad
+* disguting
+
+_Note : according to the website, some of these categories are nsfw._
+  `)
 })
 
 app.get('/category/:category', (req, res) => {
